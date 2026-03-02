2edb215825a8093fb493df366262409b327cb040
Vince <vimimoreno04@gmail.com>
2026-03-01 19:53:08 -0800

    escalation probability implemented

--- crq/fair/frequency/lef.py
+++ crq/fair/frequency/lef.py
@@ -9,20 +9,26 @@ class LEF:
         self,
         lambda_samples: np.ndarray,
         vulnerability: float,
+        escalation_prob: float
     ) -> dict:
         if not 0 <= vulnerability <= 1:
             raise ValueError("Vuln must be between 0 and 1")
         
+        if not 0 <= escalation_prob <= 1:
+            raise ValueError("Escalation probability must be between 0 and 1")
+        
         lambda_success = lambda_samples * vulnerability
 
-        successful_events = np.random.poisson(
-            lam=lambda_success
+        lambda_loss = lambda_success * escalation_prob
+
+        loss_events = np.random.poisson(
+            lam=lambda_loss
         )
 
         return {
-            "lef_mean": np.mean(successful_events),
-            "lef_distribution": successful_events,
-            "lambda_success_mean": np.mean(lambda_success)
+            "lef_mean": np.mean(loss_events),
+            "lef_distribution": loss_events,
+            "lambda_success_mean": np.mean(lambda_loss)
         }
     
 #need to build test module
